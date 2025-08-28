25b6c41cc59e8a4a696545efc26fd4e2d26641cf
tarunsahu15 <sahutarun9753162520@gmail.com>
2025-08-28 21:48:27 +0530

    my commit

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ console.log("jai shree ram , jai mata di ")
 console.log(" aum namah shivaya, aum namah shivaya ")
 console.log("hari om")
 console.log("ram ram")
+console.log(" hari om")
+
+
+
+
+
+
+
 
 
