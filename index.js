a4e33bbd10c0305e711a61ab39cd083d99701e94
tarunsahu15 <sahutarun9753162520@gmail.com>
2025-08-28 22:47:35 +0530

    mm

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log(" aum namah shivaya, aum namah shivaya ")
 console.log("hari om")
 console.log("ram ram")
 console.log("shivaya , parmeshwaraya namahaa..")
+console.log(" nagendra haraya, yogeshwaraya")
 
 
 
